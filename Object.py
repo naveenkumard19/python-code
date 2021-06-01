be28909995af640f25f76aa7d69eed2b43737c1e
Naveen Kumar <apple@Apples-MacBook-Pro-2.local>
2021-06-01 12:44:02 +0530

    Hi just created few filesi is git

---
+++ Object.py
@@ -0,0 +1,13 @@
+class Person:
+    pass
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    p1 = Person("Naveen", 44)
+
+    print(p1.name)
+    print(p1.age)
---
+++ naveen.py
@@ -0,0 +1,20 @@
+
+def triangle(base,height):
+	return int(base*height*0.5)
+
+
+print(triangle(3,4))
+
+print(triangle(7,14))
+
+print(triangle(10,10))
+
+
+
+
+
+
+
+
+
+
